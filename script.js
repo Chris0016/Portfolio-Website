45e559506d485cc9cdf5d71f930c3ebf5f5cd047
Chris0016 <57505919+Chris0016@users.noreply.github.com>
2022-07-16 21:36:59 -0400

    Modals with links added

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    
+
+// document.querySelector('#modalBtn').addEventListener('click', function () {
+// 	var myModal =  document.querySelector("#myModal");
+//     myModal.show();
+// });
+
+
+//document.querySelector('#modalBtn').innerHTML = "Hello world";
